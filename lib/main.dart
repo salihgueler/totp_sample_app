4283ac165ca88414c4cdb5d3bc2088e047ce10a4
salihgueler <muhammedsalihguler@gmail.com>
2023-09-05 12:33:36 +0300

    Update the url path and remove amplify stuff.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:totp_sample_app/sign_in_flow_application.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(
     const SignInFlowApplication(),
   );
